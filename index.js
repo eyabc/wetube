e061cea3088dd84bb00828b81b709db7b62ec0cc
eyabc <bey4314@naver.com>
2020-04-08 23:17:18 +0900

    handle route with express

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ const PORT = 4000;
 const handleListening = () => {
   console.log(`Listening on: http://localhost:${PORT}`);
 };
+
+const handleHome = (req, res) => {
+  res.send('home!')
+};
+
+app.get('/', handleHome);
+
 app.listen(PORT, handleListening);
