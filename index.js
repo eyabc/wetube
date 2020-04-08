054e584f61abfd2421038ab229efc7d8b72d125c
eyabc <bey4314@naver.com>
2020-04-09 00:10:44 +0900

    Express core: middlewares 1

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ const PORT = 4000;
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send('home!');
+const handleProfile = (req, res) => res.send('profile!');
 
-app.get('/', handleHome);
+const middleware = (req, res, next) => {
+  console.log('middle');
+  next();
+};
+
+app.get('/', middleware, handleHome);
+
+// middleware globally
+app.use(middleware)
+
+app.get('/profile', handleProfile);
+
 
 app.listen(PORT, handleListening);
