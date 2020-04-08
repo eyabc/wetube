1dcca49d204527bc005a4f2cfc3dc804ebb930ef
eyabc <bey4314@naver.com>
2020-04-09 00:37:20 +0900

    Express core: middlewares 2; helmet, morgan, body-parser, cookie-parser

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser form 'body-parser';
+
 const app = express();
 
 const PORT = 4000;
@@ -7,15 +12,17 @@ const handleListening = () => console.log(`Listening on: http://localhost:${PORT
 const handleHome = (req, res) => res.send('home!');
 const handleProfile = (req, res) => res.send('profile!');
 
-const middleware = (req, res, next) => {
-  console.log('middle');
-  next();
-};
+app.use(cookieParser());
+app.use(bodyParser().json());
+app.use(bodyParser().urlencoded({ extended: true }));
+/*
+* HTTP request logger middleware for node.js
+* options: tiny, combind, common, dev
+*/
+app.use(morgan('dev'));
 
-app.get('/', middleware, handleHome);
-
-// middleware globally
-app.use(middleware)
+/* Helmet helps you secure your Express apps by setting various HTTP headers */
+app.use(helmet());
 
 app.get('/profile', handleProfile);
 
