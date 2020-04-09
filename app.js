123099dd366443818c578365e17784ba620cd452
eyabc <bey4314@naver.com>
2020-04-09 22:26:33 +0900

    Express Core: Routing

--- app.js
+++ app.js
@@ -2,19 +2,17 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
-import bodyParser form 'body-parser';
+import bodyParser from 'body-parser';
+
+import { userRouter } from "./router";
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send('home!');
 const handleProfile = (req, res) => res.send('profile!');
 
 app.use(cookieParser());
-app.use(bodyParser().json());
-app.use(bodyParser().urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 /*
 * HTTP request logger middleware for node.js
 * options: tiny, combind, common, dev
@@ -26,5 +24,6 @@ app.use(helmet());
 
 app.get('/profile', handleProfile);
 
+app.use('/user', userRouter);
 
-app.listen(PORT, handleListening);
+export default app;
