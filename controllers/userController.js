ade1e0d4628efdf4cf6c42ee1406f28ce8bdec34
eyabc <bey4314@naver.com>
2020-04-10 14:34:58 +0900

    Template Variables in Pug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-export const join = (req, res) => res.render('Join');
-export const login = (req, res) => res.render('Login');
-export const logout = (req, res) => res.render('Logout');
-export const users = (req, res) => res.render('Users');
-export const userDetail = (req, res) => res.render('UserDetail');
-export const editProfile = (req, res) => res.render('EditProfile');
-export const changePassword = (req, res) => res.render('ChangePassword');
+export const join = (req, res) => res.render('Join', { pageTitle: "Join"} );
+export const login = (req, res) => res.render('Login', { pageTitle: "Login"});
+export const logout = (req, res) => res.render('Logout', { pageTitle: "Logout"});
+export const users = (req, res) => res.render('Users', { pageTitle: "Users"});
+export const userDetail = (req, res) => res.render('UserDetail', { pageTitle: "UserDetail"});
+export const editProfile = (req, res) => res.render('EditProfile', { pageTitle: "EditProfile"});
+export const changePassword = (req, res) => res.render('ChangePassword', { pageTitle: "ChangePassword"});
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
-export const home = (req, res) => res.render('Home');
-export const search = (req, res) => res.render('Search');
-export const videos = (req, res) => res.render('Videos');
-export const videoUpload = (req, res) => res.render('VideoUpload');
-export const videoDetail = (req, res) => res.render('VideoDetail');
-export const editVideo = (req, res) => res.render('EditVideo');
-export const deleteVideo = (req, res) => res.render('DeleteVideo');
+export const home = (req, res) => res.render('Home', { pageTitle: "Home" });
+export const search = (req, res) => res.render('Search', { pageTitle: "Search" });
+export const videos = (req, res) => res.render('Videos', { pageTitle: "Videos" });
+export const videoUpload = (req, res) => res.render('VideoUpload', { pageTitle: "VideoUpload" });
+export const videoDetail = (req, res) => res.render('VideoDetail', { pageTitle: "VideoDetail" });
+export const editVideo = (req, res) => res.render('EditVideo', { pageTitle: "EditVideo" });
+export const deleteVideo = (req, res) => res.render('DeleteVideo', { pageTitle: "DeleteVideo" });
