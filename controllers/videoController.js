dc086793ea8390116a1b164f0af7c2a3ee76c207
eyabc <bey4314@naver.com>
2020-04-10 15:01:34 +0900

    Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,8 @@
 export const home = (req, res) => res.render('Home', { pageTitle: "Home" });
-export const search = (req, res) => res.render('Search', { pageTitle: "Search" });
+export const search = (req, res) => {
+  const { query: { term } } = req;
+  res.render('Search', { pageTitle: "Search", searchingFor: term });
+};
 export const videos = (req, res) => res.render('Videos', { pageTitle: "Videos" });
 export const videoUpload = (req, res) => res.render('VideoUpload', { pageTitle: "VideoUpload" });
 export const videoDetail = (req, res) => res.render('VideoDetail', { pageTitle: "VideoDetail" });
