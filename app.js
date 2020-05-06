88807fc850a04ca560021bd5146e92a0107fa457
eyabc <bey4314@naver.com>
2020-05-06 23:00:32 +0900

    feat: webpack 기능 작업 중

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { join } from 'path'
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
@@ -10,12 +11,12 @@ import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 
-
 const app = express();
 
 app.use(helmet());
 app.set('view engine', 'pug');
 app.use('/upload', express.static('upload'));
+app.use("/static", express.static(join(__dirname, "static")));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
