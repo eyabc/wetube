5a58213540b3ca44ce894f29259909eb3a4fd55e
eyabc <bey4314@naver.com>
2020-05-06 21:14:53 +0900

    ES6 with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -12,15 +12,11 @@ const config = {
   module: {
     rules: [
       {
-        // test 와 일치하는 파일을 찾으면 use 해라 (거꾸로)
         test: /\.(scss)$/,
         use: ExtractCSS.extract([
           {
-            // webpack에게 Css를 가르침
             loader: 'css-loader',
           },
-          // 특정 플러그인들을 css 에 대해 실행시켜준다., 예를 들어 익스플로러에서 사용될 수 있게 변환해준다. Css 호환
-          // https://github.com/postcss/postcss-loader
           {
             loader: 'postcss-loader',
             options: {
@@ -29,7 +25,6 @@ const config = {
               }
             }
           },
-          // webpack에게 sass-loader 를 가르침 -> scss, sass 를 css 로 바꿔준다.
           {
             loader: 'sass-loader',
           },
